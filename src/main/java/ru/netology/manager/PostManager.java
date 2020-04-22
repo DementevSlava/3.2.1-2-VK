0a5e252339e297f0dda75d5d372be0f41dfc59bb
Dementev Slava <kursant344@gmail.com>
2020-04-22 13:17:11 +0300

    add PostManager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,19 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+
+    Post[] posts;
+
+    public Post[] search(int ownerId, String domain, String query){
+        //TODO
+        return posts;
+    }
+
+    public void delete(int ownerId, int postId){
+        //TODO
+
+    }
+
+}
